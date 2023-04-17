291cfb9b46b6bee5a3b9357f14e3a9a2f28e5653
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2023-04-17 18:41:21 -0300

    Atualizar

--- js/index.js
+++ js/index.js
@@ -1,45 +1,93 @@
-class Validator {
+ class Validator {
   constructor() {
     this.validations = [
-        'data-min-length',
+       // 'data-min-length',
     ]
   }
 
-  // iniciar a validação de todos os campos
-  validate(form) {
+   // iniciar a validação de todos os campos
+   validate(form) {
     //pegar os input
-    let inputs = form.getAttributeNames("input");
+    let inputs = form.getAttributeNames('input');
 
-    //console.log(inputs);
+     //console.log(inputs);
 
     // transformo uma HTMLCollection -> array
     let inputsArray = [...inputs];
 
-    //console.log(inputsArray);
+     //console.log(inputsArray);
+   }
+ }
+  /*
+ 
+
+   
+
+    
+
+   
 
     // loop nos input e validação meadiante ao que for encontrado
     inputsArray.forEach(function(input) {
         
-        // loop em todas as validações existentes
-        for(let i = 0; this.validations.length > i; i++){
-            if(input.getAttribute(this.validations[i]) != null){
-                console.log('achou validação');
-            }
+        // fazer validação de acordo com o atributo do input
+      for(let i = 0; this.validations.length > i; i++) {
+        if(input.getAttribute(this.validations[i]) != null) {
+
+          // limpa string para saber o método
+          let method = this.validations[i].replace("data-", "").replace("-", "");
+
+          // valor do input
+          let value = input.getAttribute(this.validations[i])
+
+          // invoca o método
+          this[method](input,value);
+
         }
+      }
 
     }, this);
   }
+
+
+// verifica se um input tem um número minimo de caracteres
+minlength(input, minValue){
+
+   let inputLength = input.value.length;
+
+   let errorMessage = `O campo precisa ter pelo menos ${minValue} caracteres`;
+
+   if(inputLength < minValue){
+    this.printMessage(input, errorMessage)
+   }
 }
+
+//metodo para imprimir msg de erro na tela
+printMessage(input, msg){
+
+    let template = document.querySelector('.erro-validation').cloneNode(true);
+
+    template.textContent = msg;
+
+    let inputParent = input.parentNode;
+
+    template.classList.remove('template');
+
+    inputParent.appendChild(template);
+}
+
+} */
 
 let form = document.getElementById("register-form");
 let submit = document.getElementById("btn-submit");
 
-let Validator = new Validator();
+let validator = new Validator();
 
 // evento que dispara as validações
-submit.addEventListener("click", function (e) {
+submit.addEventListener('click', function(e) {
+
   e.preventDefault();
 
-  Validator.validate(form);
+  validator.validate(form);
   //console.log('funcionou');
 });
