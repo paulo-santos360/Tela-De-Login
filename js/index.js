f661a53efeb2f3a966e074ab8a854e5863c37fef
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2023-04-17 17:44:01 -0300

    Atualizar

--- js/index.js
+++ js/index.js
@@ -1,23 +1,45 @@
 class Validator {
+  constructor() {
+    this.validations = [
+        'data-min-length',
+    ]
+  }
 
-    constructor(){
-        this.validations =[
+  // iniciar a validação de todos os campos
+  validate(form) {
+    //pegar os input
+    let inputs = form.getAttributeNames("input");
 
-        ]
-    }
+    //console.log(inputs);
+
+    // transformo uma HTMLCollection -> array
+    let inputsArray = [...inputs];
+
+    //console.log(inputsArray);
+
+    // loop nos input e validação meadiante ao que for encontrado
+    inputsArray.forEach(function(input) {
+        
+        // loop em todas as validações existentes
+        for(let i = 0; this.validations.length > i; i++){
+            if(input.getAttribute(this.validations[i]) != null){
+                console.log('achou validação');
+            }
+        }
+
+    }, this);
+  }
 }
-
 
 let form = document.getElementById("register-form");
 let submit = document.getElementById("btn-submit");
 
 let Validator = new Validator();
 
-
 // evento que dispara as validações
-submit.addEventListener('click', function(e){
-    e.preventDefault();
+submit.addEventListener("click", function (e) {
+  e.preventDefault();
 
-    Validator.validate(form);
-    //console.log('funcionou');
-})
+  Validator.validate(form);
+  //console.log('funcionou');
+});
